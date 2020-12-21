00a4c047fcbe8515a589dd7cd31e0638f021139d
AxonneQ <bolek.igor.94@gmail.com>
2020-12-21 15:38:21 +0000

    Add Songs and Initial Audio sampler

--- SphereVis/Assets/Scripts/AudioSampler.cs
+++ SphereVis/Assets/Scripts/AudioSampler.cs
@@ -7,16 +7,73 @@ using UnityEngine.Audio;
 public class AudioSampler : MonoBehaviour
 {
     public AudioClip track;
+    public AudioMixerGroup amgMaster;
+    private AudioSource audioSrc;
+    public static float[] spectrum;
+    public static float[] wave;
+    public static float[] bands;
+    public static float amplitude = 0;
+    public static float smoothedAmplitude = 0;
+    public static int frameSize = 512;
+    public float binWidth;
+    public float sampleRate;
     
+    private void Awake()
+    {
+        audioSrc = GetComponent<AudioSource>();
+        spectrum = new float[frameSize];
+        wave = new float[frameSize];
+        bands = new float[(int) Mathf.Log(frameSize, 2)];
+                
+        audioSrc.clip = track;
+        audioSrc.outputAudioMixerGroup = amgMaster;
+        
+        audioSrc.Play();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sampleRate = AudioSettings.outputSampleRate;
+        binWidth = AudioSettings.outputSampleRate / 2 / frameSize;
     }
+
+    void GetFrequencyBands()
+    {        
+        for (int i = 0; i < bands.Length; i++)
+        {
+            int start = (int)Mathf.Pow(2, i) - 1;
+            int width = (int)Mathf.Pow(2, i);
+            int end = start + width;
+            float average = 0;
+            for (int j = start; j < end; j++)
+            {
+                average += spectrum[j] * (j + 1);
+            }
+            average /= (float) width;
+            bands[i] = average;
+            Debug.Log(i + "\t" + start + "\t" + end + "\t" + start * binWidth + "\t" + (end * binWidth));
+        }
+
+    }
+
+    public void GetAmplitude()
+    {
+        float total = 0;
+        for(int i = 0 ; i < wave.Length ; i ++)
+            {
+        total += Mathf.Abs(wave[i]);
+        }
+        amplitude = total / wave.Length;
+        smoothedAmplitude = Mathf.Lerp(smoothedAmplitude, amplitude, Time.deltaTime * 3);
+  }
 
     // Update is called once per frame
     void Update()
     {
-        
+        audioSrc.GetSpectrumData(spectrum, 0, FFTWindow.Blackman);
+        audioSrc.GetOutputData(wave, 0);
+        GetAmplitude();
+        GetFrequencyBands();
     }
 }
--- SphereVis/Assets/Scripts/HexaSphere.cs
+++ SphereVis/Assets/Scripts/HexaSphere.cs
@@ -6,9 +6,12 @@ public class HexaSphere : MonoBehaviour
 {
     public GameObject hexagonPrefab;
     public int size = 60;
+    public float scale = 10;
+
     public int subdivisions = 4;
 
     List<GameObject> hexagons = new List<GameObject>();
+    List<Vector3> hexagonsInitPos = new List<Vector3>();
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +28,7 @@ public class HexaSphere : MonoBehaviour
             hexagons[i].transform.LookAt(transform.position);
             hexagons[i].transform.Rotate( 90, 0, 0 ) ;
             hexagons[i].transform.localScale /= 1.3f;
+            hexagonsInitPos.Add(hexagons[i].transform.position);
             i++;
         }
     }
@@ -32,10 +36,19 @@ public class HexaSphere : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        foreach(GameObject hex in hexagons) 
-        {
-            hex.transform.localScale += new Vector3(0, 0.1f, 0);
+        // foreach(GameObject hex in hexagons) 
+        // {
+        //     hex.transform.localScale += new Vector3(0, 0.1f, 0);
+        // }
+        for (int i = 0; i < hexagons.Count; i++) {
+            Vector3 ls = hexagons[i].transform.localScale;
+            ls.y = Mathf.Lerp(ls.y, 1 + (AudioSampler.bands[i] * scale), Time.deltaTime * 3.0f);
+            hexagons[i].transform.localScale = ls;
+            Vector3 pos = hexagons[i].transform.position;
+            pos.y = hexagonsInitPos[i].y + (ls.y / 2);
+            hexagons[i].transform.position = pos;
         }
+
     }
 
     Vector3[] PointsOnSphere(int n)
@@ -59,6 +72,7 @@ public class HexaSphere : MonoBehaviour
                 upts.Add(new Vector3(x, y, z));
             }
             Vector3[] pts = upts.ToArray();
+            
             return pts;
         }
 
