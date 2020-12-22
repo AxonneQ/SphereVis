dac6e6589f80e221d7298bce33e9c15fb8b967d2
AxonneQ <bolek.igor.94@gmail.com>
2020-12-22 21:53:33 +0000

    Improve the look and timings, lights

--- SphereVis/Assets/Scripts/HexaSphere.cs
+++ SphereVis/Assets/Scripts/HexaSphere.cs
@@ -5,29 +5,32 @@ using UnityEngine;
 public class HexaSphere : MonoBehaviour
 {
     public GameObject hexagonPrefab;
+    public Light columnLights;
+    public Light underLights;
     public int size = 1024;
     public float scale = 1000;
+    public float limit = 400;
     List<GameObject> hexagons = new List<GameObject>();
     List<Vector3> hexagonsInitPos = new List<Vector3>();
+    List<float> hexagonsLastScale = new List<float>();
 
     // Start is called before the first frame update
     void Start()
     {
-        float scaling = 16;
+        float scaling = 14;
         Vector3[] pts = PointsOnSphere(size);
         int i = 0;
 
         for (int j = pts.Length - 1; j >= 0; j--)
         {
-            Debug.Log(pts.Length);
-
             hexagons.Add(GameObject.Instantiate(hexagonPrefab));
             hexagons[i].transform.parent = transform;
-            hexagons[i].transform.position = pts[j] * scaling;
+            hexagons[i].transform.localPosition = pts[j] * scaling;
             hexagons[i].transform.LookAt(transform.position);
             hexagons[i].transform.Rotate(90, 0, 0);
             hexagons[i].transform.localScale /= 1.3f;
-            hexagonsInitPos.Add(hexagons[i].transform.position);
+            hexagonsInitPos.Add(hexagons[i].transform.localPosition);
+            hexagonsLastScale.Add(hexagons[i].transform.localScale.y);
             i++;
         }
     }
@@ -39,10 +42,21 @@ public class HexaSphere : MonoBehaviour
         {
             hexagons[i].transform.localPosition = hexagonsInitPos[i];
             Vector3 lscale = hexagons[i].transform.localScale;
-            lscale.y = Mathf.Lerp(lscale.y, 1 + (Mathf.Abs(AudioSampler.spectrum[i]) * scale), Time.deltaTime * 3.0f);
+            if(hexagonsLastScale[i] > lscale.y) {
+                lscale.y = Mathf.Lerp(hexagonsLastScale[i], lscale.y, Time.deltaTime * 0.5f);
+            } else {
+                if((Mathf.Abs(AudioSampler.spectrum[i]) * scale) > limit) {
+                    lscale.y = Mathf.Lerp(lscale.y, 1 + (Mathf.Abs(AudioSampler.spectrum[i]) * scale / 3), Time.deltaTime * 3.0f);
+                } else {
+                    lscale.y = Mathf.Lerp(lscale.y, 1 + (Mathf.Abs(AudioSampler.spectrum[i]) * scale * 3), Time.deltaTime * 3.0f);
+                }
+            }
+            
             ScaleAround(hexagons[i], hexagons[i].transform.localPosition + new Vector3(lscale.x, lscale.y / 2, lscale.z), lscale);
-            hexagons[i].transform.position = hexagonsInitPos[i];
+            hexagons[i].transform.localPosition = hexagonsInitPos[i];
         }
+        underLights.intensity = Mathf.Lerp(underLights.intensity, AudioSampler.amplitude * 20, 0.05f);
+        columnLights.intensity = Mathf.Lerp(columnLights.intensity, AudioSampler.amplitude * 150, Time.deltaTime * 3.0f);
     }
 
     public void ScaleAround(GameObject target, Vector3 pivot, Vector3 newScale)
@@ -54,8 +68,7 @@ public class HexaSphere : MonoBehaviour
             newScale.z / target.transform.localScale.z );
         pivotDelta.Scale(scaleFactor);
         target.transform.position = pivot + pivotDelta;
-    
-        //scale
+
         target.transform.localScale = newScale;
     }
 
@@ -64,11 +77,7 @@ public class HexaSphere : MonoBehaviour
         List<Vector3> upts = new List<Vector3>();
         float inc = Mathf.PI * (3 - Mathf.Sqrt(5));
         float off = 2.0f / n;
-        float x = 0;
-        float y = 0;
-        float z = 0;
-        float r = 0;
-        float phi = 0;
+        float x, y, z, r, phi = 0;
 
         for (var k = 0; k < n; k++)
         {
